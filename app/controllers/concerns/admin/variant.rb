bd2da8a358ba07c679cba7dae27357953ae6962b
Roberto Scinocca <roberto.scinocca@gmail.com>
2022-04-08 13:34:00 +0200

    split application headers in separated files for better customizzation

--- app/controllers/concerns/admin/variant.rb
+++ app/controllers/concerns/admin/variant.rb
@@ -6,21 +6,21 @@ module Admin::Variant
 
     private
 
-      def detect_device_format
-        case request.user_agent
-        when /Turbo Native/i
-          request.variant = :native
-        when /iPad/i
-          request.variant = :tablet
-        when /iPhone/i
-          request.variant = :phone
-        when /Android/i && /mobile/i
-          request.variant = :phone
-        when /Android/i
-          request.variant = :tablet
-        when /Windows Phone/i
-          request.variant = :phone
-        end
+    def detect_device_format
+      case request.user_agent
+      when /Turbo Native/i
+        request.variant = :native
+      when /iPad/i
+        request.variant = :tablet
+      when /iPhone/i
+        request.variant = :phone
+      when /Android/i && /mobile/i
+        request.variant = :phone
+      when /Android/i
+        request.variant = :tablet
+      when /Windows Phone/i
+        request.variant = :phone
       end
+    end
   end
 end
